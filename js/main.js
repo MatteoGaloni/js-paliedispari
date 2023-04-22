16fb62bddfd47506a9575e03e4d9db83be8c2a52
MatteoGaloni <mtgaloni@gmail.com>
2023-04-22 17:26:42 +0200

    feat Pari e dispari: add prompt user

--- js/main.js
+++ js/main.js
@@ -1,31 +1,45 @@
+// ***PALIDROMA***
+
 // ************Variables**********
+// let parola;
+// parola = prompt("Inserisci una parola")
+// let parolaReverse = ""
+// // ****provo a passare parola come argomento****
+// let risultato = ispalindromo(parola);
+// console.log(risultato)
+// // console.log(risultato);
 
-let parola;
-parola = prompt("Inserisci una parola")
-let parolaReverse= ""
-let risultato = ispalindromo(parola);
-// console.log(risultato);
+// // **********Condition********
 
-// **********Condition********
+// if (risultato === true) {
+//     alert("La parola che hai inserito è un palindromo")
+// } else {
+//     alert("Mi dispiace, la parola che hai inserito non è un palindromo")
+// }
 
-if (risultato === true) {
-    alert("La parola che hai inserito è un palindromo")
-} else {
-    alert("Mi dispiace, la parola che hai inserito non è un palindromo")
-}
+// // ************Function**************
 
-// ************Function**************
+// function ispalindromo(word) {
+//     for (let c = word.length - 1; c >= 0; c--) {
+//         console.log(c)
+//         parolaReverse += parola[c]
+//     }
+//     // console.log(parolaReverse)
+//     if (parola == parolaReverse) {
+//         return true
+//         // risultato = alert("La parola è un palindromo")
+//     }
+//     return false
+// }
 
-function ispalindromo() {
-    for (let c = parola.length -1; c >= 0; c--) {
-        parolaReverse += parola[c]             
-    }
-    // console.log(parolaReverse)
-    if (parola == parolaReverse) {
-        return true
-        // risultato = alert("La parola è un palindromo")
-    }
-    return false    
-}
+
+// ***Pari e Dispari***
+
+let sceltaUtente = prompt("Ciao, scegli se pari o dispari")
+console.log(sceltaUtente)
+
+let numeroUtente = prompt("Scegli un numero da 1 a 5")
+
+
 
 
